e6a0860fe3ed1d2ccb3d25caede155b37f780c18
Dave Barr <dave.barr@atomicsmash.co.uk>
2022-02-18 15:28:42 +0000

    Add mix.scripts allowing all files in a dir

---
+++ dist/js/all.js
@@ -0,0 +1,3 @@
+alert("hello world");
+
+alert("hello world & Dave");
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,29 +3,30 @@ let fs = require("fs");
 let path = require("path");
 
 // Multi JS
-fs.readdirSync("src/js/").forEach((fileName) => mix.js(`src/js/${fileName}`, "dist/js"));
+//fs.readdirSync("src/js/").forEach((fileName) => mix.js(`src/js/${fileName}`, "dist/js"));
 
-// mix.scripts(["src/js/app.js", "src/js/app-home.js"], "dist/js/all.js");
+//mix.scripts(["src/js/app.js", "src/js/app-home.js"], "dist/js/all.js");
+mix.scripts("src/js", "dist/js/all.js");
 
 // Multi SASS
-var files = fs.readdirSync("src/sass");
-for (var i = 0; i < files.length; i++) {
-  if (path.extname(files[i]) == ".scss") {
-    mix.sass("src/sass/" + files[i], "dist/css", {
-      sassOptions: {
-        outputStyle: "compressed",
-      },
-    });
-  }
-}
+// var files = fs.readdirSync("src/sass");
+// for (var i = 0; i < files.length; i++) {
+//   if (path.extname(files[i]) == ".scss") {
+//     mix.sass("src/sass/" + files[i], "dist/css", {
+//       sassOptions: {
+//         outputStyle: "compressed",
+//       },
+//     });
+//   }
+// }
 
 mix
   // .js("src/js/app.js", "js")
-  // .sass("src/sass/**/*.scss", "css", {
-  //   sassOptions: {
-  //     outputStyle: "compressed",
-  //   },
-  // })
-  // .setPublicPath("dist")
+  .sass("src/sass/styles.scss", "css", {
+    sassOptions: {
+      outputStyle: "compressed",
+    },
+  })
+  .setPublicPath("dist")
   .version()
   .browserSync("http://laravel-mix-demo.test");
