5ef90c296202ff29f421642a96d06868c3a425a2
Dave Barr <dave.barr@atomicsmash.co.uk>
2022-02-25 15:57:27 +0000

    Updates 25-02-22

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,52 @@
 let mix = require("laravel-mix");
-let fs = require("fs");
-let path = require("path");
+const devUrl = "http://laravel-mix-demo.test";
 
+mix.scripts("src/js", "dist/js/all.js");
+
+// localhost:3000
+mix.browserSync({
+  proxy: "http://laravel-mix-demo.test",
+  files: [
+    //Files for watching
+    "./index.html",
+    "./src/**/*",
+  ],
+});
+
+mix
+  .sass("src/sass/home.scss", "css", {
+    sassOptions: {
+      // processCssUrls: false,
+      outputStyle: "compressed",
+    },
+  })
+  .setPublicPath("dist")
+  .version();
+// .browserSync(devUrl);
+// .browserSync({
+//   proxy: devUrl,
+// });
+
+mix
+  .sass("src/sass/styles.scss", "css", {
+    sassOptions: {
+      // processCssUrls: false,
+      outputStyle: "compressed",
+    },
+  })
+  .setPublicPath("dist")
+  .version();
+// .browserSync(devUrl);
+// .browserSync({
+//   proxy: devUrl,
+// });
+
+// ------------------------------------------------------
+// let fs = require("fs");
+// let path = require("path");
 // Multi JS
 //fs.readdirSync("src/js/").forEach((fileName) => mix.js(`src/js/${fileName}`, "dist/js"));
-
 //mix.scripts(["src/js/app.js", "src/js/app-home.js"], "dist/js/all.js");
-mix.scripts("src/js", "dist/js/all.js");
 
 // Multi SASS
 // var files = fs.readdirSync("src/sass");
@@ -19,14 +59,3 @@ mix.scripts("src/js", "dist/js/all.js");
 //     });
 //   }
 // }
-
-mix
-  // .js("src/js/app.js", "js")
-  .sass("src/sass/styles.scss", "css", {
-    sassOptions: {
-      outputStyle: "compressed",
-    },
-  })
-  .setPublicPath("dist")
-  .version()
-  .browserSync("http://laravel-mix-demo.test");
